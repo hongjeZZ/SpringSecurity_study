659ea20d7279d50059bee043d6573fa47bf7e5ba
hongjeZZ <anhongje98@naver.com>
2024-09-20 16:49:33 +0900

    add explanation WebSecurityConfig & logout 기능

--- src/main/java/com/prgrms/devcourse/config/WebSecurityConfig.java
+++ src/main/java/com/prgrms/devcourse/config/WebSecurityConfig.java
@@ -6,12 +6,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    // 로그인 가능한 계정 추가
+    /*
+    로그인 가능한 계정 추가
+    Spring Security 5에서는 DelegatingPasswordEncoder 가 기본 PasswordEncoder 로 사용된다.
+    DelegatingPasswordEncoder 클래스는 패스워드 해시 알고리즘별로 PasswordEncoder 를 제공하는 객체이다 (실제 Encoder 를 감싸는 객체)
+    해시 알고리즘별 PasswordEncoder 선택을 위해 패스워드 앞에 prefix 를 추가해줘야 한다. (prefix 생략 시 bcrypt 적용)
+    default -> {bcrypt}, passwordEncoder 사용 x -> {noop}, 나머지 -> {pbkdf2}, {sha256} 등등
+
+    +) UserDetailsPasswordService 인터페이스 구현체를 통해 최초 로그인 1회 성공 시,
+    {noop} 타입에서 → {bcrypt} 타입으로 PasswordEncoder 가 변경된다.
+    +) DelegatingPasswordEncoder 사용이 필요 없다면 BCryptPasswordEncoder 클래스를 명시적으로 Bean 선언하자
+    */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -34,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             /*
-            spring security 는 /me 경로로 사용자가 들어왔을 때, "me" view 경로로 보내기 전 권한 여부를 파악하고
+            spring security 는 "/me" 경로로 사용자가 들어왔을 때, "me" view 경로로 보내기 전 권한 여부를 파악하고
             스스로 login 페이지로 redirection 처리한다.
             */
             .authorizeRequests()
@@ -45,7 +56,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/")
                 .permitAll()
                 .and()
-            .rememberMe()
-                .tokenValiditySeconds(300);
+            /*
+            "/logout" 경로로 접근 시 로그아웃 실행, "/" 로그아웃 성공 시 리다이렉션
+            invalidateHttpSession(true) -> 로그아웃 시 해당 사용자의 세션을 invalidate
+            clearAuthentication(true) -> 로그아웃된 사용자의 SecurityContext 의 Authentication 을 null 로 초기화
+            +) 추가로 아래 설정은 전부 default 값임
+            */
+            .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+            ;
     }
 }
